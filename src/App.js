4b37e602c2700bf3b8774dc15fe8727519aaf8ce
friscolie <68372390+friscolie@users.noreply.github.com>
2022-02-23 21:58:29 +0700

    Create Header components and arrange to landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import "./App.css";
 import "./assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">hai</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
