eea6721f4f5c91a44f7f3a7872175e7a52c6d8fa
friscolie <68372390+friscolie@users.noreply.github.com>
2022-02-24 21:39:41 +0700

    finished components mostpicked and categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+
+import landingData from "json/landingPage.json";
+import MostPick from "parts/MostPick";
+import Categories from "parts/Categories";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingData.hero} />
+        <MostPick
+          refMostPicked={this.refMostPicked}
+          data={landingData.mostPicked}
+        />
+        <Categories data={landingData.categories} />
       </>
     );
   }
