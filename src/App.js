e306b4932ec05ac4f254590002c105ee0caad86d
friscolie <68372390+friscolie@users.noreply.github.com>
2022-02-27 19:56:49 +0700

    booking details completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./assets/scss/style.scss";
-import LandingPage from "pages/LandingPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Example from "pages/Example";
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+
+import "./assets/scss/style.scss";
+import Checkout from "pages/Checkout";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage} />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/example" component={Example} />
       </Router>
     </div>
   );
---
+++ src/pages/Example.js
@@ -0,0 +1,23 @@
+import Breadcrumb from "elements/Breadcrumb";
+import React, { Component } from "react";
+
+export default class Example extends Component {
+  render() {
+    const breadcrumbList = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
+    return (
+      <div className="container">
+        <div
+          className="row align-items-center justify-content-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="col-auto">
+            <Breadcrumb data={breadcrumbList} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
